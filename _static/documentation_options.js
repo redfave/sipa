f16f593736d2359c90947c086445c34b63675cbc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-22 21:12:42 +0000

    Deploy agdsn/sipa to github.com/agdsn/sipa.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0-29-gf173681',
+    VERSION: '1.1.0-37-g1903ba8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
